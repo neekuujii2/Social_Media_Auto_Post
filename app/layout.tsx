6e9acd59d7a903243353b22f709f214d10d23883
neekuujii2 <neerajkumar75260@gmail.com>
2026-05-30 22:24:48 +0530

    Add and update the packages

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
+import { QueryProvider } from "@/components/query-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Lemon.ai | Social Media Scheduling",
+  description: "Create AI-powered social media scheduling for every platform in seconds. Lemon.ai is a platform that allows you to create social media scheduling for every platform in seconds.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.className} h-full antialiased`}
+      // style={
+      //   {
+      //     "--font-sans": geistSans.style.fontFamily,
+      //     "--font-mono": geistMono.style.fontFamily,
+      //   } as React.CSSProperties
+      // }
+    >
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>
+          <QueryProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <TooltipProvider>
+                {children}
+              </TooltipProvider>
+
+              <Toaster  richColors/>
+            </ThemeProvider>
+
+          </QueryProvider>
+
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
